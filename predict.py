b02a190453df6b1e64145b3bd5684246938ad613
hyunju <dlguswn7768@gmail.com>
2021-05-06 06:10:41 +0900

    걍 짜증난다 이제

--- predict.py
+++ predict.py
@@ -39,7 +39,7 @@ def predict_unseen_data(category_str, question_str):
 	eng_category = translator.translate(kor_category, dest='en')
 	eng_question = translator.translate(kor_question, dest='en')
 	# print(eng_category.text)
-
+	test_examples[0]['category'] = 'Sales, subscription'
 	test_examples[0]['category'] = eng_category.text
 	test_examples[0]['question'] = eng_question.text
 	# test_examples = test_examples.text
